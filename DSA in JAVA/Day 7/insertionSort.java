cf3efd77580a8c7024e28cbf65f7563bbf273e97
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2024-01-08 22:51:32 +0530

    insertion Sort | day 7

---
+++ DSA in JAVA/Day 7/insertionSort.java
@@ -0,0 +1,15 @@
+public class insertionSort { // change class name to 'Solution' on coding ninjas studio.
+    public static void insertionSort(int[] arr, int size) {
+        //Your code goes here
+        for (int i = 1; i < size; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
